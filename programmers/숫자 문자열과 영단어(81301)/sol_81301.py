6a8927803f942e89b3132cf5c4379fbd27b9d37b
Jiyong-Jeon/laptop <jjy5136@naver.com>
2022-08-14 22:37:49 +0900

    sol programmers 81301

---
+++ programmers/숫자 문자열과 영단어(81301)/sol_81301.py
@@ -0,0 +1,6 @@
+import re
+def solution(s):
+    words = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for key in words.keys():
+        s = re.sub(key, words[key],s)
+    return int(s)
