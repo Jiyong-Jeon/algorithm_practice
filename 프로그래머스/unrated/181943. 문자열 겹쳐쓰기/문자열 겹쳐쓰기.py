b655e5556579cf715094d2d755b6e7d29c2e2128
Jiyong-Jeon <68497156+Jiyong-Jeon@users.noreply.github.com>
2023-05-19 01:42:39 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,2 @@
+def solution(my_string, overwrite_string, s):
+    return my_string[:s]+overwrite_string if s+len(overwrite_string) > len(my_string) else my_string[:s]+overwrite_string+my_string[s+len(overwrite_string):]
