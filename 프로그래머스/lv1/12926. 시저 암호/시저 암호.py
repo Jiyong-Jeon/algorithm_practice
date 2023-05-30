fa49e7412c553caeb65af895e10e52c28e276f1c
Jiyong-Jeon <68497156+Jiyong-Jeon@users.noreply.github.com>
2023-05-30 12:04:48 +0900

    [level 1] Title: 시저 암호, Time: 2.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,14 +1,13 @@
 def solution(s, n):
     answer = ''
     for c in s:
-        c_num = ord(c)
-        if c_num >= 97 and c_num <= 122:
-            temp = 97
-        elif c_num >= 65 and c_num <= 90:
-            temp = 65
+        if c.islower():
+            temp = ord("a")
+        elif c.isupper():
+            temp = ord("A")
         else:
             answer += ' '
             continue
-        c_num = (c_num - temp + n) % 26
+        c_num = (ord(c) - temp + n) % 26
         answer += chr(c_num + temp)
     return answer
